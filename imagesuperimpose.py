4fc1c704519714d218ee2f883bf23ee0a083dd37
test1230-lab <64430532+test1230-lab@users.noreply.github.com>
2020-08-05 13:30:54 -0400

    Update imagesuperimpose.py
    faster

--- imagesuperimpose.py
+++ imagesuperimpose.py
@@ -1,22 +1,20 @@
 import glob
-from PIL import Image, ImageEnhance
+import numpy as np
+import cv2
 
-print("the number of files in each dir must be the same")
 z = 0
 x = ((z),'.png')
 images = glob.glob("faces/*.jpg")
-print ("number of files in dir faces:",len(images))
 images2 = glob.glob("hands/*.png")
-print ("number of files in dir hands:",len(images2))
 for image,image2 in zip(images, images2):
-    with open(image, 'rb') as file:
-        with open(image2, 'rb') as file2:
-            x = ((z),'.png')
-            img2 = Image.open(file2)
-            img = Image.open(file)
-            img.paste(img2, (0,0), img2)
-            y = str(x)
-            img.save("{}.png".format(z))
-            z = (z) + 1
-            print("IMAGE:", (z),"/",len(images))
-            
+	img = cv2.imread(image, -1)
+	img2 = cv2.imread(image2, -1)
+	h, w, c = img2.shape
+	result = np.zeros((h, w, 3), np.uint8)
+	x = ((z),'.png')
+	img2 = cv2.resize(img2,(1024,1024), interpolation = cv2.INTER_CUBIC)
+	alpha = np.tile(img2[..., 3][..., None], 3) / 255.0
+	result = (1. - alpha) * img[..., :3] + alpha * img2[..., :3]
+	cv2.imwrite(image.split('/')[1],result)
+	z = (z) + 1
+	print("IMAGE:", (z),"/",len(images))
